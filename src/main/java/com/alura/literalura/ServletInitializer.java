1932222606afd13b987c520f308ef98407333e15
salo <salomontilla@outlook.com>
2024-10-30 23:46:37 -0500

    Agrega archivos

---
+++ src/main/java/com/alura/literalura/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.alura.literalura;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LiteraluraApplication.class);
+	}
+
+}
